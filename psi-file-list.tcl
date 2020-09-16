eeb1cc40d51fb010f78030fe77ef3c5b87559aef
till straumann <till.straumann@alumni.tu-berlin.de>
2020-09-16 23:09:33 +0200

    Added tcl snippet listing relevant files for PSI

---
+++ psi-file-list.tcl
@@ -0,0 +1,15 @@
+set xvc_support_fw_files { \
+  "rtl/Surf/StdRtlPkg.vhd" \
+  "rtl/Surf/AxiStreamPkg.vhd" \
+  "rtl/Axis2Jtag/AxisToJtagPkg.vhd" \
+  "rtl/Axis2Jtag/JtagSerDesCore.vhd" \
+  "rtl/Axis2Jtag/AxiStreamSelector.vhd" \
+  "rtl/Axis2Jtag/AxisToJtagCore.vhd" \
+  "rtl/Axis2Jtag/AxisToJtag.vhd" \
+  "rtl/JtagTap/JtagTapFSM.vhd" \
+  "rtl/JtagTap/JtagTapIR.vhd" \
+  "rtl/Jtag2BSCAN/Jtag2BSCAN.vhd" \
+  "rtl/BusIf/Tmem/Axis2TmemFifo.vhd" \
+  "rtl/BusIf/Tmem/Tmem2BSCANWrapper.vhd" \
+  "rtl/BusIf/Tmem/Tmem2BSCANConstraints.ucf" \
+}
