8907b0b619d1216678e5fd893ac1d7550b2cc16c
till straumann <till.straumann@alumni.tu-berlin.de>
2020-09-17 19:29:32 +0200

    psi-file-list.tcl: added proc that lets the user add all relevant RTL and UCF files to an ISE project

--- psi-file-list.tcl
+++ psi-file-list.tcl
@@ -13,3 +13,14 @@ set xvc_support_fw_files { \
   "rtl/BusIf/Tmem/Tmem2BSCANWrapper.vhd" \
   "rtl/BusIf/Tmem/Tmem2BSCANConstraints.ucf" \
 }
+
+set xvc_support_location "[file dirname [info script]]"
+
+proc xvc_support_add_srcs { pre } {
+  global xvc_support_fw_files
+  global xvc_support_location
+  set xvcSupPath "[file dirname [info script]]"
+  foreach f $xvc_support_fw_files {
+    xfile add "$pre$xvc_support_location/$f"
+  }
+}
